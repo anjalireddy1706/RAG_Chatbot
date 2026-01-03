f2454e9493856a425aed2284eb743378ffd2fe95
Anjali <2976766R@student.gla.ac.uk>
2026-01-03 20:22:57 +0000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="rag-chatbot",
+    version="0.1.0",
+    author="Anjali Reddy",
+    author_email="anjali.reddyy1706@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
